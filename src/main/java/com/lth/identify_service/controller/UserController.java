efd25be7b07e010d3d7f08854e0431e2a404b3c4
LTH <hieule1235@gmail.com>
2025-02-07 08:38:45 +0700

    initial project

---
+++ src/main/java/com/lth/identify_service/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.lth.identify_service.controller;
+
+import com.lth.identify_service.dto.request.UserCreationRequest;
+import com.lth.identify_service.entity.User;
+import com.lth.identify_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    User createUser(@RequestBody UserCreationRequest request) {
+        return userService.createUser(request);
+    }
+}
---
+++ src/main/java/com/lth/identify_service/service/UserService.java
@@ -0,0 +1,26 @@
+package com.lth.identify_service.service;
+
+import com.lth.identify_service.dto.request.UserCreationRequest;
+import com.lth.identify_service.entity.User;
+import com.lth.identify_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+
+    }
+}
